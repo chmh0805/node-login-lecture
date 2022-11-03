8004c1c76ff063682bcd9ddbf8998aafbab7839f
Minhyuk Jung <chmh0805@gmail.com>
2022-11-04 00:35:38 +0900

    2. Separate Router

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const http = require("http");
-const app = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html; charset=utf-8",
-  });
-  if (req.url === "/") {
-    res.end("this is root page.");
-  } else if (req.url === "/login") {
-    res.end("this is login page.");
-  }
+const express = require("express");
+const app = express();
+const home = require("./routes/home");
+
+// App Setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> Method to register Middleware.
+
+app.listen(3000, () => {
+  console.log("server start listening on port 3000");
 });
-
-app.listen(3001, () => {
-  console.log("Running Server With http");
-});
-
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => {
-//   res.send('this is root page.')
-// })
-
-// app.get('/login', (req, res) => {
-//   res.send('this is login page.')
-// })
-
-// app.listen(3000, () => {
-//   console.log('server start listening on port 3000')
-// })
