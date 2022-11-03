25eac980ef96af7cc30cfee366cf0de8bfc2d2a9
Minhyuk Jung <chmh0805@gmail.com>
2022-11-03 23:29:31 +0900

    1-1. Run Server With http

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const app = express()
+const http = require("http");
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+  });
+  if (req.url === "/") {
+    res.end("this is root page.");
+  } else if (req.url === "/login") {
+    res.end("this is login page.");
+  }
+});
 
-app.get('/', (req, res) => {
-  res.send('this is root page.')
-})
+app.listen(3001, () => {
+  console.log("Running Server With http");
+});
 
-app.get('/login', (req, res) => {
-  res.send('this is login page.')
-})
+// const express = require('express')
+// const app = express()
 
-app.listen(3000, () => {
-  console.log('server start listening on port 3000')
-})
+// app.get('/', (req, res) => {
+//   res.send('this is root page.')
+// })
+
+// app.get('/login', (req, res) => {
+//   res.send('this is login page.')
+// })
+
+// app.listen(3000, () => {
+//   console.log('server start listening on port 3000')
+// })
