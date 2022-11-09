563183572a6b7a57abd8091fa0f097a09826d04e
Minhyuk Jung <chmh0805@gmail.com>
2022-11-09 22:38:47 +0900

    Optimize Directory Structure

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@
 
 const express = require("express");
 const app = express();
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // App Setting
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> Method to register Middleware.
