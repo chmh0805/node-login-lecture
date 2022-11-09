ddd6e63c2e478d44352b75103f9c8c71dad69ebe
Minhyuk Jung <chmh0805@gmail.com>
2022-11-09 22:43:40 +0900

    6. Make Javascript File for Frontend

--- app/app.js
+++ app/app.js
@@ -9,5 +9,6 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> Method to register Middleware.
+app.use(express.static(`${__dirname}/src/public`)); // register 'src/public' as static directory.
 
 module.exports = app;
