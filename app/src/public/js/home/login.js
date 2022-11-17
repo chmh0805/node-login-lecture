17399b86eac3514fde2e9b8a1a742cc9ab3afb11
Minhyuk Jung <chmh0805@gmail.com>
2022-11-18 00:41:51 +0900

    Fix: set type button to Login.ejs Login Button

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,9 +4,25 @@ const id = document.querySelector("#userId");
 const pwd = document.querySelector("#userPwd");
 const loginBtn = document.querySelector("#login-btn");
 
+window.addEventListener("keypress", enterEventListenerToLogin);
 loginBtn.addEventListener("click", login);
 
+function enterEventListenerToLogin(e) {
+  if (e.charCode === 13) {
+    e.preventDefault();
+    login();
+  }
+}
+
 function login() {
+  if (!id.value) {
+    alert("Please Enter ID.");
+    return;
+  }
+  if (!pwd.value) {
+    alert("Please Enter Password.");
+    return;
+  }
   const reqBody = {
     id: id.value,
     pwd: pwd.value,
